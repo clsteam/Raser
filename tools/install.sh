875607ee885d1ca9fbe621d94c76ba0d505c850c
clsteam <bioaurora@webmail.hzau.edu.cn>
2020-08-06 21:40:45 +0800

    version1.0.0

--- tools/install.sh
+++ tools/install.sh
@@ -1,2 +1,16 @@
-cd sra-tools
+current_path=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+
+# sra-tools
+cd ncbi/ngs
 ./configure --prefix=./
+make -C ngs-sdk
+make -C ngs-java
+cd ../ncbi-vdb
+./configure --prefix=./
+make
+make install
+cd ../sra-tools
+./configure --prefix=./
+make
+make install
+# echo "export PATH=${current_path}/ncbi/sra-tools/bin:"'$PATH' >> ~/.bashrc
